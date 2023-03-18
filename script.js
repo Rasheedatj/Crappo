89d31efb275b68f172f9e86e5ea2d0e56339624a
Rasheedat Jinadu <jinaduleoluwa@gmal.com>
2023-03-18 19:58:59 +0100

    refactored purpleBoxes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const btn = document.getElementById("menu_btn");
 const nav = document.getElementById("menu");
 const purpleBox = document.querySelectorAll(".flex_boxes .box");
-const over = document.querySelectorAll("#box");
 
 btn.addEventListener("click", () => {
   nav.classList.toggle("open");
@@ -11,11 +10,15 @@ btn.addEventListener("click", () => {
 purpleBox.forEach((box) =>
   box.addEventListener("click", () => {
     box.classList.toggle("colorActive");
-  })
-);
+    box.classList.toggle("overstyle");
 
-over.forEach((arrow) =>
-  arrow.addEventListener("click", () => {
-    arrow.classList.toggle("overstyle");
+    if (box.previousElementSibling) {
+      box.previousElementSibling.classList.remove("colorActive");
+      box.previousElementSibling.classList.remove("overstyle");
+    }
+    if (box.nextElementSibling) {
+      box.nextElementSibling.classList.remove("colorActive");
+      box.nextElementSibling.classList.remove("overstyle");
+    }
   })
 );

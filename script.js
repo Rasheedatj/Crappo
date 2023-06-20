065f205c1ea0d6bc2db92f53e2d6848f72ed3983
Rasheedat Jinadu <jinaduleoluwa@gmal.com>
2023-06-20 18:36:59 +0100

    javascript code cleanup

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-const btn = document.getElementById("menu_btn");
-const nav = document.getElementById("menu");
-const purpleBox = document.querySelectorAll(".flex_boxes .box");
+const btn = document.getElementById('menu_btn');
+const nav = document.getElementById('menu');
+const parent = document.querySelector('.flex_boxes');
+const purpleBox = document.querySelectorAll('.flex_boxes .box');
 
-btn.addEventListener("click", () => {
-  nav.classList.toggle("open");
-  btn.classList.toggle("open_btn");
+btn.addEventListener('click', () => {
+  nav.classList.toggle('open');
+  btn.classList.toggle('open_btn');
 });
 
-purpleBox.forEach((box) =>
-  box.addEventListener("click", () => {
-    box.classList.toggle("colorActive");
-    box.classList.toggle("overstyle");
+function onClickBox(e) {
+  purpleBox.forEach((box) => {
+    box.classList.remove('colorActive');
+    box.classList.remove('overstyle');
+  });
 
-    if (box.previousElementSibling) {
-      box.previousElementSibling.classList.remove("colorActive");
-      box.previousElementSibling.classList.remove("overstyle");
-    }
-    if (box.nextElementSibling) {
-      box.nextElementSibling.classList.remove("colorActive");
-      box.nextElementSibling.classList.remove("overstyle");
-    }
-  })
-);
+  e.target.classList.toggle('colorActive');
+  e.target.classList.toggle('overstyle');
+}
+
+parent.addEventListener('click', onClickBox);
